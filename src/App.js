2b2d69d565ff7f23aab06f72a6e48bff40cea4d6
Erick Lima <souzalimaerick5@gmail.com>
2023-09-06 18:55:33 -0300

    teste erro

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import {FiSearch} from 'react-icons/fi'
+function App() {
+  return (
+    <div className="container">
+      <h1 className="title">Buscador de CEP</h1>
+    <div className="containerInput">
+      <input
+       type="text"
+       placeholder="Digite seu CEP..."
+       />
+    
+      <button className="buttonSearch">
+        <FiSearch size = {25} color = "#0000"/>
+      </button>
+    
+    </div>
+
+    </div>
+  );
+}
